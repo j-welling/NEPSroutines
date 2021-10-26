ea62c321228049b10e8a903e36c193e491438d29
Anna Scharl <anna.scharl@lifbi.de>
2021-10-26 11:49:59 +0200

    V0.1.0; dimensionality summary output now contains correlation/variance matrix

--- R/dimensionality_analysis.R
+++ R/dimensionality_analysis.R
@@ -4,13 +4,18 @@
 #'                    (1) the responses. y in {0, 1} for binary data and y in
 #'                      {0, 1, ... k-1} for polytomous responses with k categories
 #'                    (2) ID_t: column indicating ID of participants
-#' @param vars      data.frame with all variables as rows with at least the following columns:
+#' @param vars      data.frame with all variables as rows with at least the
+#'                  following columns:
 #'                  items: contains name for each item (scored and unscored)
-#'                  poly: identifies all polytomous (?) items (logical)
-#'                  scored: identifies all scored items (logical)
+#'                  and a column identifying item subsets for the analyses and
+#'                  one for the item scoring
 #' @param items     character. contains name of variable (boolean) in vars that
 #'                    indicates which items to use for analysis.
-#' @param dim       character vector. contains names of all dimension variables that shall be analyzed
+#' @param scoring   character. Contains the name of the variable in vars that
+#'                    indicates the variable used for scoring. Defaults to
+#'                    "scoring"
+#' @param dim       character vector. contains names of all dimension variables
+#'                    that shall be analyzed
 #' @param valid     character string. defines name of boolean variable in dat,
 #'                    indicating (in)valid cases.
 #' @param irtmodel  the choice of irtmodel as passed to the TAM function
@@ -23,9 +28,9 @@
 #' @export
 #' @importFrom stats AIC BIC logLik
 
-dimension_analysis <- function(resp, vars, items, dim = NULL, valid = NULL,
-                               irtmodel = "PCM2", maxiter = 10, snodes = 5,
-                               verbose = FALSE) {
+dimension_analysis <- function(resp, vars, items, scoring = "scoring",
+                               dim = NULL, valid = NULL, irtmodel = "PCM2",
+                               maxiter = 10, snodes = 5, verbose = FALSE) {
 
     # Select only valid cases
     resp <- only_valid(resp, valid = valid)
@@ -35,10 +40,8 @@ dimension_analysis <- function(resp, vars, items, dim = NULL, valid = NULL,
     check_pid(pid)
 
     # Select only indicated items and convert mvs
-    resp <- prepare_resp(resp, vars = vars, items = items, convert = TRUE, without_valid = TRUE)
-
-    # Identify polytomous items
-    sel <- as.integer(apply(resp, 2, max, na.rm = TRUE) > 1)
+    resp <- prepare_resp(resp, vars = vars, items = items, convert = TRUE,
+                         without_valid = TRUE)
 
     # Create object for results
     dimensionality <- list()
@@ -46,14 +49,14 @@ dimension_analysis <- function(resp, vars, items, dim = NULL, valid = NULL,
     ## 1D reference model
 
     # Create Q matrix
-    Q <- matrix( 1 , nrow = ncol(resp), ncol = 1)
-    Q[sel, 1] <- 0.5    # score of 0.5 for polytomous items
+    Q <- as.matrix(vars[[scoring]][vars[[items]]])
 
     # Compute dimensional model
     dimensionality$uni <- TAM::tam.mml(
-        resp = resp, pid = pid, Q = Q, irtmodel = irtmodel,
-        control = list(maxiter = maxiter, snodes = snodes), verbose = verbose
+        resp = resp, pid = pid, Q = Q, irtmodel = irtmodel, verbose = verbose,
+        control = list(maxiter = maxiter, snodes = snodes)
     )
+    message("Finished unidimensional reference model.")
 
     if (!is.null(dim)) {
 
@@ -61,18 +64,20 @@ dimension_analysis <- function(resp, vars, items, dim = NULL, valid = NULL,
 
             # Create Q matrix
             v <- as.double(vars[vars[[items]], d])
-            dimensions <- unique(v)
+            dimensions <- sort(unique(v))
             Q <- matrix(0, nrow = length(v), ncol = length(dimensions))
             for (i in dimensions) {
                 Q[v == i, i] <- 1
             }
-            Q[sel, ] <- Q[sel, ] * 0.5
+            Q <- Q * vars[[scoring]][vars[[items]]]
 
             # Compute dimensional model
             dimensionality[[d]] <- TAM::tam.mml(
                 resp = resp, pid = pid, Q = Q, irtmodel = irtmodel,
-                control = list(maxiter = maxiter, snodes = snodes), verbose = verbose
+                control = list(maxiter = maxiter, snodes = snodes),
+                verbose = verbose
             )
+            message("Finished ", d," model.")
         }
     }
 
@@ -91,7 +96,8 @@ dimension_analysis <- function(resp, vars, items, dim = NULL, valid = NULL,
 #'   data.frame as Rdata; third, the information criteria are saved in an excel
 #'   file). Please note that the path is relative to the current working path
 #'   set by here::i_am(). Defaults to NULL (not stored)
-#' @param overwrite boolean; indicates whether to overwrite existing file when saving table.
+#' @param overwrite boolean; indicates whether to overwrite existing file when
+#'   saving table.
 #'
 #' @return data.frame of summary results
 #'
@@ -99,22 +105,22 @@ dimension_analysis <- function(resp, vars, items, dim = NULL, valid = NULL,
 #' @export
 #'
 
-dimension_summary <- function(dimensionality, print = TRUE, save_at = NULL,
+dimension_summary <- function(dimensionality, save_at = NULL,
                               overwrite = FALSE) {
 
     dim <- names(dimensionality)
+    dimsum <- list()
     gof <- data.frame(Stat = c("loglik", "AIC", "BIC"))
 
     for (d in dim) {
         gof[[d]] <- c(logLik(dimensionality[[d]]),
-                        AIC(dimensionality[[d]]),
-                        BIC(dimensionality[[d]]))
+                      AIC(dimensionality[[d]]),
+                      BIC(dimensionality[[d]]))
+        tmp <- dimensionality[[d]]$variance
+        dimsum[[paste("Cor-Var", d)]] <- cov2cor(tmp)
+        diag(dimsum[[paste("Cor-Var", d)]]) <- diag(tmp)
     }
-
-    if (print) {
-        message("Information criteria for dimensionality analyses \n")
-        print(gof)
-    }
+    dimsum[["Goodness Of fit"]] <- gof
 
     if (!is.null(save_at)) {
 
@@ -125,11 +131,11 @@ dimension_summary <- function(dimensionality, print = TRUE, save_at = NULL,
         save(gof,
              file = here::here(paste0(save_at, "/dimensionality_fit.Rdata")))
         openxlsx::write.xlsx(
-            gof,
-            file = here::here(paste0(save_at, "/dimensionality_fit.xlsx")),
+            dimsum,
+            file = here::here(paste0(save_at, "/dimensionality_summary.xlsx")),
             showNA = FALSE, overwrite = overwrite
         )
     }
 
-    return(gof)
+    return(dimsum)
 }
