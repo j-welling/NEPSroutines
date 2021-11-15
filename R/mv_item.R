e2e4a346c63e77c0145f2a6fd5ffe66c606a14b9
Jana Welling <jana.welling@lifbi.de>
2021-11-15 17:40:14 +0100

    v0.1.0; created print_mvi_results() as well as in-package-functions create_mvlist() and mv_summary()

--- R/mv_item.R
+++ R/mv_item.R
@@ -68,22 +68,15 @@ mvi_analysis <- function(resp, vars, items, position = NULL,
   # Create results
   if (is.null(grouping)) {
 
-    # Number of valid responses and position
-    mvlist <- data.frame(items = vars_c$items,
-                         position = vars_c[[position]],
-                         N = colSums(apply(resp_c, 2, function(x) {!(x %in% mvs)}))
-                         )
-
-    # Determine percentage of missing values for each missing type
-    results <- data.frame(mvi_calc(resp = resp_c, mvs = mvs, digits = digits))
-    results$items <- row.names(results)
-    mvlist <- merge(mvlist, results, by = 'items', all = TRUE)
+    # Create list with results
+    mvlist <- create_mvlist(items = vars_c$items,
+                            position = vars_c[[position]],
+                            responses = resp_c,
+                            mvs = mvs,
+                            digits = digits)
 
     # Summary across items
-    mvsum <- t(apply(mvlist[ , -1], 2, function(x) {
-      round(c(mean(x), sd(x), median(x), range(x)[1], range(x)[2]), digits)
-    }))
-    colnames(mvsum) <- c("Mean", "Median", "SD", "Min", "Max")
+    mvsum <- mv_summary(mvlist[ , -c(1:2)])
 
   } else {
 
@@ -99,25 +92,19 @@ mvi_analysis <- function(resp, vars, items, position = NULL,
       # Number of valid responses and position
       if (length(position) == 1) {
         pos <- vars_c[[position]]
-        } else {
-          pos <- vars_c[[position[g]]]
-        }
+      } else {
+        pos <- vars_c[[position[g]]]
+      }
 
-      mvlist[[g]] <- data.frame(items = vars_c$items[!is.na(pos)],
-                                position = na.omit(pos),
-                                N = colSums(apply(resp_g, 2,
-                                function(x) !(x %in% mvs))))
-
-      # Determine percentage of missing values for each missing type
-      results <- data.frame(mvi_calc(resp = resp_g, mvs = mvs, digits = digits))
-      results$items <- row.names(results)
-      mvlist[[g]] <- merge(mvlist[[g]], results, by = 'items')
+      # Create list with results
+      mvlist[[g]] <- create_mvlist(items = vars_c$items[!is.na(pos)],
+                                   position = na.omit(pos),
+                                   responses = resp_g,
+                                   mvs = mvs,
+                                   digits = digits)
 
       # Summary across items
-      mvsum[[g]] <- t(apply(mvlist[[g]][ , -c(1:2)], 2, function(x) {
-        round(c(mean(x), sd(x), median(x), range(x)[1], range(x)[2]), digits)
-      }))
-      colnames(mvsum[[g]]) <- c("Mean", "Median", "SD", "Min", "Max")
+      mvsum[[g]] <- mv_summary(mvlist[[g]][ , -c(1:2)])
     }
 
     if (length(position) > 1) {
@@ -150,10 +137,7 @@ mvi_analysis <- function(resp, vars, items, position = NULL,
       mvlist$all <- cbind(mvlist$all, results)
 
       # Summary across items
-      mvsum$all <- t(apply(mvlist$all[ , -1], 2, function(x) {
-        round(c(mean(x), sd(x), median(x), range(x)[1], range(x)[2]), digits)
-      }))
-      colnames(mvsum$all) <- c("Mean", "Median", "SD", "Min", "Max")
+      mvsum$all <- mv_summary(mvlist$all[ , -1])
     }
   }
 
@@ -511,4 +495,94 @@ mvi_calc <- function(resp, mvs, digits = 2) {
   result$ALL <- mvi_perc(resp = resp, mvs = mvs, digits = digits)
 
   return(result)
+}
+
+
+#' Create list with missing values per item
+#'
+#' @param items     character vector; contains names of items
+#' @param position  integer vector; contains position of items
+#' @param responses data.frame; contains item responses {0,1}
+#' with only the items administered to and the cases of the designated group
+#' @param mvs       named vector with definition of one user-defined missing values
+#' @param digits    number of decimals for rounding
+#'
+#' @return   list with results of missing values per item.
+#' @noRd
+
+create_mvlist <- function(items, position, responses, mvs, digits = 2) {
+
+  if (length(items) != length(position) |
+      ncol(responses) != length(items) |
+      ncol(responses) != length(position)) {
+        stop("Number of items in data.frame responses, in vector items and in vector position do not match. ",
+        "Please provide matching arguments to function create_mvlist().")
+      }
+
+  # Create list
+  mvlist <- data.frame(items = items,
+                       position = position,
+                       N = colSums(apply(resp, 2, function(x) !(x %in% mvs))))
+
+  # Merge with percentage of missing values for each missing type
+  results <- data.frame(mvi_calc(resp = resp, mvs = mvs, digits = digits))
+  results$items <- row.names(results)
+  mvlist <- merge(mvlist, results, by = 'items')
+
+  # Return list
+  return(mvlist)
+}
+
+
+#' MVI summary
+#'
+#' @param mvlist  list with missing values per item as returned by function create_mvlist()
+#'
+#' @return  data.frame with mean, median, SD, min and max of missing values
+#' @noRd
+
+mvi_summary <- function(mvlist) {
+  mvsum <- data.frame(t(apply(mvlist, 2, function(x) {
+    round(c(mean(x), sd(x), median(x), range(x)[1], range(x)[2]), digits)
+  })))
+  colnames(mvsum) <- c("Mean", "Median", "SD", "Min", "Max")
+  return(mvsum)
+}
+
+#' Print MVI results
+#'
+#' @param mv_i      list; return object of mvi_analysis(). mvi_analysis$list must contain
+#'                  for each group and all groups together the name of the
+#'                  user-defined missing values that are to be
+#'                  analyzed and the names of the items as its row names.
+#' @param labels    lables for different types of user-defined missing values
+#' @param grouping  character vector. contains names of groups (e.g. 'easy and 'difficult')
+#'                  as used in resp.
+#'
+#' @export
+
+print_mvi_results <- function(mv_i, labels, grouping = NULL) {
+  if (is.null(grouping)) {
+    for (lbl in labels) {
+      mv_min <- min(mv_i$list[[names(lbl)]], na.rm = TRUE)
+      mv_max <- max(mv_i$list[[names(lbl)]], na.rm = TRUE)
+      item_min <- mv_i$list$items[mv_i$list[[names(lbl)]] == mv_min]
+      item_max <- mv_i$list$items[mv_i$list[[names(lbl)]] == mv_max]
+      message("The number of ", lbl, " items varied between ",
+              mv_min, " % (item ", if(length(item_min) == 1) {print(item_min)}, ") and ",
+              mv_max, " % (item ", item_max, ").")
+    }
+  } else {
+    for (g in grouping) {
+      for (lbl in labels) {
+        mv_min <- min(mv_i$list[[g]][[names(lbl)]], na.rm = TRUE)
+        mv_max <- max(mv_i$list[[g]][[names(lbl)]], na.rm = TRUE)
+        item_min <- mv_i$list[[g]]$items[mv_i$list[[g]][[names(lbl)]] == mv_min]
+        item_max <- mv_i$list[[g]]$items[mv_i$list[[g]][[names(lbl)]] == mv_max]
+        message("The number of ", lbl, " items in the ", g, "test version varied between ",
+                mv_min, " % (item ", if(length(item_min) == 1) {print(item_min)}, ") and ",
+                mv_max, " % (item ", item_max, ").")
+      }
+    }
+  }
 }
